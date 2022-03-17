734be31fd165d2659eb27aed38332cbec5911682
MriLevi <74299880+MriLevi@users.noreply.github.com>
2022-03-17 23:18:28 +0100

    a lot of stuff done

--- neuron_network.py
+++ neuron_network.py
@@ -1,16 +1,23 @@
 class Neuron:
 
-    def __init__(self, threshold, activation_function, weights):
+    def __init__(self, threshold, activation_function, weights, learning_rate=0.01):
+
         self.bias = -threshold
-        self.threshold = threshold
         self.activation_function = activation_function
         self.weights = weights
-        self.error_total = 0
-        self.updatecount = 0
-        self.MSE = None
+        self.learning_rate = learning_rate
+        self.inputs = []
+        self.output = None
+
+        self.error = 0
+        self.gradients = None
+        self.target = None
+        self.derivative = None
+        self.delta_weights = None
+        self.delta_bias = None
 
     def __str__(self):
-        return f'bias: {self.bias} - threshold: {self.threshold} - activation_function: {self.activation_function.__name__}\n' \
+        return f'bias: {self.bias} - activation_function: {self.activation_function.__name__}\n' \
                f'weights: {self.weights}'
 
     def dot(self, x1, x2):
@@ -20,30 +27,42 @@ class Neuron:
 
     def determine_output(self, inputs):
         """Determines the output of a neuron by adding the dot product of the weights and inputs and adding bias"""
-        return self.activation_function(self.bias + self.dot(self.weights, inputs))
+        self.inputs = inputs
+        self.output = self.activation_function(self.bias + self.dot(self.weights, inputs))
+        return self.output
 
-    def update(self, inputs, target, learning_rate=0.1):
-        """This function allows os to train/fit our neuron by following the perceptron learning rule."""
+    def hidden_error(self, weights, errors):
+        """Determine the error for hidden neuron"""
+        self.error = self.derivative * self.dot(weights, errors)
 
-        y = self.determine_output(inputs)
-        error = target - y
-        if error != 0:
-            delta_weights = []
-            for w,i in zip(self.weights, inputs):
-                delta_weights.append(w + (learning_rate * error * i))
-            delta_bias = learning_rate * error
-            self.weights = delta_weights
-            self.bias += delta_bias
-            self.threshold = -self.bias
-        #here we keep track of the updates we've done and the squared error, so we can easily determine the loss later
-        self.updatecount += 1
-        self.error_total += error**2
+    def output_error(self, target):
+        """Determine the  error for output neuron"""
+        self.target = target
+        self.error = self.derivative * -(target - self.output)
 
-    def loss(self):
-        """This function keeps track of the MSE by simply dividing the sum of squared errors,
-        divided by the number of updates already done."""
-        self.MSE = self.error_total/self.updatecount
-        return self.MSE
+    def update(self):
+        """Adjust the weights and bias of the neuron"""
+        new_weights = [i - j for i, j in zip(self.weights, self.delta_weights)]
+        self.weights = new_weights
+        self.bias -= self.delta_bias
+
+    def neuron_function_activation(self, weights=None, errors=None, target=None):
+        """This neuron activation function runs the necessary functions.
+        We determine derivative, gradients, new weights and new biases in this function.
+        We also determine either hidden or output errors.
+        If a target is specified, we use the output_error function as that indicates that the neuron is an output neuron.
+        Else, we use the hidden error function."""
+
+        self.derivative = self.output * (1 - self.output)
+
+        if target is not None:
+            self.target = target
+            self.output_error(target)
+        else:
+            self.hidden_error(weights, errors)
+        self.gradients = [i * self.error for i in self.inputs]
+        self.delta_weights = [self.learning_rate * i for i in self.gradients]
+        self.delta_bias = self.learning_rate * self.error
 
 
 class NeuronLayer:
@@ -51,25 +70,83 @@ class NeuronLayer:
     def __init__(self, neurons):
         self.neurons = neurons
 
-    def determine_outputs(self, inputs):
-        """Determines the output for each neuron by calling determine_output() for each neuron
-           Returns a list of output values."""
+    def feed_forward(self, inputs):
+        """Determine the outputs for neurons in this layer"""
         outputs = [neuron.determine_output(inputs) for neuron in self.neurons]
         return outputs
+
+    def layer_errors(self):
+        """Calculate all the errors in this layer"""
+        return [neuron.error for neuron in self.neurons]
+
+    def layer_update(self):
+        """Update all weigths and biases in this layer by calling neuron.update()"""
+        for neuron in self.neurons:
+            neuron.update()
 
 
 class NeuronNetwork:
 
     def __init__(self, neuron_layers):
         self.neuron_layers = neuron_layers
+        self.all_outputs = []
+        self.network_losses = []
 
     def feed_forward(self, initial_input):
         """Feed forward determines the output for each layer by using the output of the previous as input for the next
             It determines the first layers outputs outside of the loop to be able to then loop over the remaining
             layers. Finally, it returns the output of the final layer.
             NOTE: The first layer must always have its inputs defined."""
-        inputs = self.neuron_layers[0].determine_outputs(initial_input)
+        inputs = self.neuron_layers[0].feed_forward(initial_input)  # input layer
         for neuron_layer in self.neuron_layers[1:]:
-            inputs = neuron_layer.determine_outputs(inputs)
-        result = inputs
-        return result
+            inputs = neuron_layer.feed_forward(inputs)
+        self.all_outputs = [initial_input]
+        self.all_outputs.append(inputs)
+
+    def loss(self, targets):
+        """Calculate the loss for a network"""
+        loss = 0
+        for i in range(len(targets)):
+            loss += (targets[i] - self.all_outputs[-1][i]) ** 2
+        self.network_losses.append(loss)
+
+    def total_loss(self, inputs, targets):
+        """This function determines the total loss"""
+        for input, target in zip(inputs, targets):
+            self.feed_forward(input)
+            self.loss(target)
+        return sum(self.network_losses) / len(self.network_losses)
+
+    def update(self):
+        """Update every layers weights and biases"""
+        for layer in self.neuron_layers:
+            layer.layer_update()
+
+    def train(self, inputs, targets, epochs):
+        """This function trains the neural network"""
+
+        count = 0
+        loss_target = 0.01
+        while count < epochs and self.total_loss(inputs, targets) > loss_target:
+            for input, target in zip(inputs, targets):
+                self.feed_forward(input)
+
+                output_layer = self.neuron_layers[-1].neurons
+                hidden_layer = self.neuron_layers[:-1]
+
+                # output layer
+                for i, neuron in enumerate(output_layer):
+                    neuron.neuron_function_activation(weights=None, errors=None, target=target[i])
+
+                # calculate hidden neurons
+                for layer in reversed(range(len(hidden_layer))):
+                    for index in range(len(hidden_layer[layer].neurons)):
+                        weights = [neuron.weights[index] for neuron in self.neuron_layers[layer + 1].neurons]
+                        errors = self.neuron_layers[layer + 1].layer_errors()
+                        # activate the neuron
+                        self.neuron_layers[layer].neurons[index].neuron_function_activation(weights, errors)
+
+                for layer in self.neuron_layers:
+                    layer.layer_update()
+                self.network_losses = [] #reset the network losses
+            count += 1 #keep track of epochs
--- tests.py
+++ tests.py
@@ -2,36 +2,21 @@ import activation as act
 import neuron_network as nn
 import itertools
 import unittest
-from collections import defaultdict
+import random as rand
 
-'''2.aIk heb hieronder de tests staan. 4 tests falen, dat zijn de tests zoals ik ze voor de perceptron ook heb gebruikt.
- Deze tests falen omdat er uit sigmoid eigenlijk nooit 0.0 komt, En bijna nooit 1.0. Ik heb ook tests aangemaakt waar
- ik de outputs van de gates afrond - dan werken ze wel weer.
- Dit verklaard dus ook waarom 4 van de 5 tests falen. 
- Verder heb ik helemaal onderaan de test staan voor een halfadder. Ook hier ronden we de outputs af.'''
 
-class Tests(unittest.TestCase):
-    def test_and_gate(self):
-        """the and gate outputs 1 if both x1 and x2 (inputs) are 1, else it outputs 0"""
+class MyTestCase(unittest.TestCase):
+    def test_AND(self):
         inputs = list(itertools.product([0, 1], repeat=2))
-        weights = [1, 1]
-        threshold = 1
-        and_neuron = nn.Neuron(threshold, act.sigmoid, weights)
+        targets = [[0],[0],[0],[1]]
+        weights = [rand.random(), rand.random()]
+        threshold = rand.random()
+        epochs=1500
+        and_neuron = nn.Neuron(threshold, act.sigmoid, weights, learning_rate=0.1)
+        and_layer = nn.NeuronLayer([and_neuron])
+        and_network = nn.NeuronNetwork([and_layer])
 
-        for i in inputs:
-            if i == (1, 1):
-                self.assertEqual(1, and_neuron.determine_output(i),
-                                 f"UNROUNDED AND gate: Result should be 1, since input is {i}")
-            else:
-                self.assertEqual(0, and_neuron.determine_output(i),
-                                 f"UNROUNDED AND gate: Result should be 0, since input is {i}")
-
-    def test_rounded_and_gate(self):
-        """De AND gate werkt niet als we de output niet afronden. Deze versie rond de output af en dan werkt hij wel."""
-        inputs = list(itertools.product([0, 1], repeat=2))
-        weights = [1, 1]
-        threshold = 1
-        and_neuron = nn.Neuron(threshold, act.sigmoid, weights)
+        and_network.train(inputs, targets, epochs)
 
         for i in inputs:
             if i == (1, 1):
@@ -40,105 +25,64 @@ class Tests(unittest.TestCase):
             else:
                 self.assertEqual(0, round(and_neuron.determine_output(i)),
                                  f"AND gate: Result should be 0, since input is {i}")
-    def test_or_gate(self):
-        """The or gate returns 1 if either x1 or x2 is 1, or if both are 1."""
-        inputs = list(itertools.product([0, 1], repeat=2))
-        weights = [2, 2]
-        threshold = 1
-        or_neuron = nn.Neuron(threshold, act.sigmoid, weights)
-        for i in inputs:
-            if i == (0, 0):
-                self.assertEqual(0, or_neuron.determine_output(i),
-                                 f"OR gate: Result should be 0, since input is {i}")
-            else:
-                self.assertEqual(1, or_neuron.determine_output(i),
-                                 f"OR gate: Result should be 1, since input is {i}")
-    def test_rounded_or_gate(self):
-        """Ook de OR-gate werkt alleen als we de values afronden"""
-        inputs = list(itertools.product([0, 1], repeat=2))
-        weights = [2, 2]
-        threshold = 1
-        or_neuron = nn.Neuron(threshold, act.sigmoid, weights)
-        for i in inputs:
-            if i == (0, 0):
-                self.assertEqual(0, round(or_neuron.determine_output(i)),
-                                 f"ROUNDED OR gate: Result should be 0, since input is {i}")
-            else:
-                self.assertEqual(1, round(or_neuron.determine_output(i)),
-                                 f"ROUNDED OR gate: Result should be 1, since input is {i}")
-    def test_not_gate(self):
-        """The NOT gate returns 1 if inputs are x1 and x2 are 0, else returns 0"""
-        inputs = list(itertools.product([0, 1], repeat=2))
-        weights = [-1, -1]
-        threshold = -1
-        not_neuron = nn.Neuron(threshold, act.sigmoid, weights)
-        for i in inputs:
-            if i == (0, 0):
-                self.assertEqual(1, not_neuron.determine_output(i),
-                                 f"NOT gate: Result should be 1, since input is {i}")
-            else:
-                self.assertEqual(0, not_neuron.determine_output(i),
-                                 f"NOT gate: Result should be 0, since input is {i}")
-    def test_rounded_not_gate(self):
-        """The NOT gate returns 1 if inputs are x1 and x2 are 0, else returns 0"""
-        inputs = list(itertools.product([0, 1], repeat=2))
-        weights = [-1, -1]
-        threshold = -1
-        not_neuron = nn.Neuron(threshold, act.sigmoid, weights)
-        for i in inputs:
-            if i == (0, 0):
-                self.assertEqual(1, round(not_neuron.determine_output(i)),
-                                 f"ROUNDED NOT gate: Result should be 1, since input is {i}")
-            else:
-                self.assertEqual(0, round(not_neuron.determine_output(i)),
-                                 f"ROUNDED NOT gate: Result should be 0, since input is {i}")
-    def test_nor_gate(self):
-        """The NOR gate returns 1 only if x1 and x2 are 0, else returns 0"""
-        inputs = list(itertools.product([0, 1], repeat=3))
-        weights = [-1, -1, -1]
-        threshold = -1  # bias == -threshold
-        nor_neuron = nn.Neuron(threshold, act.sigmoid, weights)
-        for i in inputs:
-            if i == (0, 0, 0):
-                self.assertEqual(1, nor_neuron.determine_output(i), f"UNROUNDED NOR: Result should be 1, since input is {i}")
-            else:
-                self.assertEqual(0, nor_neuron.determine_output(i), f"UNROUNDED NOR: Result should be 1, since input is {i}")
 
-    def test_rounded_nor_gate(self):
-        """The NOR gate returns 1 only if x1 and x2 are 0, else returns 0"""
-        inputs = list(itertools.product([0, 1], repeat=3))
-        weights = [-1, -1, -1]
-        threshold = -1  # bias == -threshold
-        nor_neuron = nn.Neuron(threshold, act.sigmoid, weights)
+    def test_XOR(self):
+        learning_rate = 0.5
+        #layer1
+        neuron1_1 = nn.Neuron(rand.random(), act.sigmoid, [rand.random(),rand.random()], learning_rate)
+        neuron1_2 = nn.Neuron(rand.random(), act.sigmoid, [rand.random(),rand.random()], learning_rate)
+        layer1 = nn.NeuronLayer([neuron1_1, neuron1_2])
+
+        #layer2
+        neuron2_1 = nn.Neuron(rand.random(), act.sigmoid, [rand.random(),rand.random()], learning_rate)
+        layer2 = nn.NeuronLayer([neuron2_1])
+        XOR_network = nn.NeuronNetwork([layer1, layer2])
+
+        inputs = list(itertools.product([0, 1], repeat=2))
+        targets = [[0], [1], [1], [0]]
+        epochs = 10000
+
+        XOR_network.train(inputs, targets, epochs)
+
         for i in inputs:
-            if i == (0, 0, 0):
-                self.assertEqual(1, round(nor_neuron.determine_output(i)), f"ROUNDED NOR: Result should be 1, since input is {i}")
+            XOR_network.feed_forward(i)
+            if i in [(0,1), (1,0)]:
+                self.assertEqual(1, round(XOR_network.all_outputs[-1][0]))
             else:
-                self.assertEqual(0, round(nor_neuron.determine_output(i)), f"ROUNDED NOR: Result should be 1, since input is {i}")
+                self.assertEqual(0, round(XOR_network.all_outputs[-1][0]))
 
-    def test_half_adder(self):
-        inputs = list(itertools.product([0,1], repeat=2))
-        #initialize the gates for layer 1
-        or_neuron = nn.Neuron(1, act.sigmoid, [2,2])
-        nand_neuron = nn.Neuron(-1.5, act.sigmoid, [-1,-1])
-        and_neuron = nn.Neuron(1.5, act.sigmoid, [1,1])
-        layer1 = nn.NeuronLayer([or_neuron, nand_neuron, and_neuron])
 
-        #layer2, output layer
-        sum_neuron = nn.Neuron(1.5, act.sigmoid, [1.5,1.5,-1])
-        carry_neuron = nn.Neuron(1, act.sigmoid, [-0.5,-0.5,3])
-        layer2 = nn.NeuronLayer([sum_neuron, carry_neuron])
+
+
+    def test_Half_adder(self):
+        inputs = list(itertools.product([0, 1], repeat=2))
+        targets = [[0,0], [1,0], [1,0], [0,1]]
+        learning_rate = 1
+        epochs = 10000
+        # initialize the gates for layer 1
+        neuron1_1 = nn.Neuron(rand.random(), act.sigmoid, [rand.random(),rand.random()], learning_rate)
+        neuron1_2 = nn.Neuron(rand.random(), act.sigmoid, [1,1], learning_rate)
+        neuron1_3 = nn.Neuron(rand.random(), act.sigmoid, [rand.random(),rand.random()], learning_rate)
+        layer1 = nn.NeuronLayer([neuron1_1, neuron1_2, neuron1_3])
+
+        # layer2, output layer
+        neuron2_1 = nn.Neuron(rand.random(), act.sigmoid, [rand.random(),rand.random(), rand.random()], learning_rate)
+        neuron2_2 = nn.Neuron(rand.random(), act.sigmoid, [rand.random(),rand.random(), rand.random()], learning_rate)
+        layer2 = nn.NeuronLayer([neuron2_1, neuron2_2])
 
         half_adder = nn.NeuronNetwork([layer1, layer2])
+        half_adder.train(inputs, targets, epochs)
 
         # half adder should return [0,0] for input [0,0], [1,0] for inputs [1,0] [0,1], and [0,1] for input [1,1].
         for i in inputs:
-            if i == (0,0):
-                self.assertEqual([0,0], [round(i) for i in half_adder.feed_forward(i)])
-            elif i in [(0,1),(1,0)]:
-                self.assertEqual([1,0], [round(i) for i in half_adder.feed_forward(i)])
+            half_adder.feed_forward(i)
+            if i == (0, 0):
+                self.assertEqual([0, 0], [round(i) for i in half_adder.all_outputs[-1]])
+            elif i in [(0, 1), (1, 0)]:
+                self.assertEqual([1, 0], [round(i) for i in half_adder.all_outputs[-1]])
             else:
-                self.assertEqual([0,1], [round(i) for i in half_adder.feed_forward(i)])
+                self.assertEqual([0, 1], [round(i) for i in half_adder.all_outputs[-1]])
+
 
 
 if __name__ == '__main__':
